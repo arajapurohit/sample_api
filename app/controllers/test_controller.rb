2d2ed170624208e431773b714255675f45cc657b
amar <arajapurohit@qwinixtech.com>
2014-09-15 11:38:31 +0530

    [master] index

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,5 +1,9 @@
 class TestController < ApplicationController
 
+  def index
+    @schools = SchoolSection.all
+  end
+
   def new
     @school = SchoolSection.new
   end
@@ -8,14 +12,17 @@ class TestController < ApplicationController
     @school = SchoolSection.new(school_params)
 
     if @school.save
-      redirect_to root_path
-
+      render 'show'
     else
       redirect_to root_path
     end
 
   end
 
+  def show
+    @school = SchoolSection.find(params[:id])
+    redirect_to test_path
+  end
 
 private
     def school_params
