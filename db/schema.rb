5420a530d6123bfb31cf3bfca1a69bebd526dbb4
amar <arajapurohit@qwinixtech.com>
2014-10-07 17:13:03 +0530

    migrate the table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140911070107) do
+ActiveRecord::Schema.define(version: 20141007104800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20140911070107) do
   create_table "teacher_sections", force: true do |t|
     t.string   "name"
     t.integer  "school_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
